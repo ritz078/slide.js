cdd330c9b61f786722ecf36a7df7dff25393af78
Ritesh Kumar <rkritesh078@gmail.com>
2015-10-30 21:28:38 +0530

    chore(travis): removed travis.yml and the travis task from gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -89,6 +89,5 @@ module.exports = function(grunt) {
 
     grunt.registerTask("build", ["concat", "uglify", "sass", "cssmin"]);
     grunt.registerTask("default", ["jshint", "build"]);
-    grunt.registerTask("travis", ["default"]);
 
 };
